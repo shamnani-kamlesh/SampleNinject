83ba408e1009f4d793998ed71b8d6b2127b2425d
Kamlesh Shamnani <30417442+shamnani-kamlesh@users.noreply.github.com>
2018-02-02 08:06:00 +0530

    Add files via upload
    initial commint-missed projects

---
+++ BLL/FruitBLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+using Contracts;
+
+namespace BLL
+{
+    public class FruitBLL : IFruitBLL
+    {
+        private IFruitRepository _fruitRefpository;
+
+        public FruitBLL(IFruitRepository fruitRefpository)
+        {
+            _fruitRefpository = fruitRefpository;
+        }
+
+        public List<Fruit> GetFruits()
+        {
+            return _fruitRefpository.GetFruits();
+        }
+    }
+}
